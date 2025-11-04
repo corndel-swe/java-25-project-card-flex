993a781e7868f4e55f0304dea98e0c573d26278c
Frank Yamoah <frankyamoah94@gmail.com>
2025-11-04 14:35:21 +0000

    updated card

--- src/main/java/org/project/cardflex/Repository/CardRepository.java
+++ src/main/java/org/project/cardflex/Repository/CardRepository.java
@@ -9,69 +9,67 @@ import java.util.Random;
 
 public class CardRepository {
 
-//   Display Credit Card Type to Users
-    public static Cards newCreditCard (int userId, String cardName) throws SQLException {
-        var query = "INSERT INTO Cards (accountNumber, creditLimit, APR, refreshDate, cardName, balance), VALUES (? ? ? ? ? ?)";
+    //   Display Credit Card Type to Users
+    public static Cards newCreditCard(int userId, String cardName) throws SQLException {
+        var query = "INSERT INTO Cards (account_number, credit_limit, apr, refresh_date, card_name, balance), VALUES (? ? ? ? ? ?)";
         try (var con = DB.getConnection();
-             var stmt = con.prepareStatement(query))
-             {
-                 Random r = new Random ();
-                 float APR = 0;
-                 int creditLimit = 0;
-                 if (Boolean.parseBoolean(cardName ="Gold".toLowerCase())){
-                 APR = 10.70F;
-                 creditLimit = 5000;}
-             else if (Boolean.parseBoolean(cardName = "Platinum".toLowerCase())) {
-                 APR = 15.10F;
-                 creditLimit = 15000;}
-
-             else {
-                     APR = 18.00F;
-                     creditLimit = 25000;}
-
-                int accountNumber =  r.nextInt(10000);
-                stmt.setInt(1, accountNumber); // Check to see if it's unique
-                stmt.setFloat(2, creditLimit);
-                stmt.setFloat(3, APR);
-                stmt.setString(4, "1");
-                stmt.setString(5, cardName);
-                stmt.setInt(6, 0);
-                try (var rs = stmt.executeQuery();) {
-                    var accountNum = rs.getInt(3);
-                    var creditLim = rs.getInt(4);
-                    var balance = rs.getInt(5);
-                    var apr = rs.getFloat(6);
-                    var startDate = rs.getString(7);
-                    return new Cards(accountNum, creditLim, apr, cardName, startDate, balance);
-                }
-
-             }
-        }
-    // Locating Existing Cards Based off User ID
-    public static List<Cards> findCardsByUserID () throws SQLException {
-        var query = "SELECT * FROM Cards WHERE userID = ?";
-        try (var con = DB.getConnection();
-             var stmt = con.createStatement();
-             var rs = stmt.executeQuery(query)) {
-
-            var cards = new ArrayList<Cards>();
-            while (rs.next()) {
-                var id = rs.getInt("id");
-                var userId = rs.getInt("userId");
-                var accountNumber = rs.getInt("accountNumber");
-                var creditLimit = rs.getFloat("creditLimit");
-                var APR = rs.getFloat("APR");
-                var startDate = rs.getString("startDate");
-                var refreshDate = rs.getString("refreshDate");
-                var cardName = rs.getString("cardName");
-
-
-                cards.add(new Cards(id, userId, accountNumber, creditLimit, APR, startDate, refreshDate, cardName));
+             var stmt = con.prepareStatement(query)) {
+            Random r = new Random();
+            float APR = 0;
+            int creditLimit = 0;
+            if (Boolean.parseBoolean(cardName = "Gold".toLowerCase())) {
+                APR = 10.70F;
+                creditLimit = 5000;
+            } else if (Boolean.parseBoolean(cardName = "Platinum".toLowerCase())) {
+                APR = 15.10F;
+                creditLimit = 15000;
+            } else {
+                APR = 18.00F;
+                creditLimit = 25000;
             }
-            return cards;
+
+            int accountNumber = r.nextInt(10000);
+            stmt.setInt(1, accountNumber); // Check to see if it's unique
+            stmt.setFloat(2, creditLimit);
+            stmt.setFloat(3, APR);
+            stmt.setString(4, "1");
+            stmt.setString(5, cardName);
+            stmt.setInt(6, 0);
+            try (var rs = stmt.executeQuery();) {
+                var accountNum = rs.getInt(3);
+                var creditLim = rs.getInt(4);
+                var balance = rs.getInt(5);
+                var apr = rs.getFloat(6);
+                var startDate = rs.getString(7);
+                return new Cards(accountNum, creditLim, apr, cardName, startDate, balance);
+            }
 
         }
+    }
 
+    // Locating Existing Cards Based off User ID
+    public static List<Cards> findCardsByUserID(int userId) throws SQLException {
+        var query = "SELECT * FROM Cards WHERE user_id = ?";
+        try (var con = DB.getConnection();
+             var stmt = con.prepareStatement(query)) {
+            stmt.setInt(1, userId);
+            try (var rs = stmt.executeQuery()) {
+
+                var cards = new ArrayList<Cards>();
+                while (rs.next()) {
+                    var accountNumber = rs.getInt("account_number");
+                    var creditLimit = rs.getFloat("credit_limit");
+                    var APR = rs.getFloat("apr");
+                    var startDate = rs.getString("start_date");
+                    var refreshDate = rs.getString("refresh_date");
+                    var cardName = rs.getString("card_name");
+                    var balance = rs.getInt("balance");
+                    cards.add(new Cards(userId, accountNumber, creditLimit, APR, cardName, startDate, refreshDate, balance));
+                }
+                return cards;
+
+            }
+        }
     }
 }
 
