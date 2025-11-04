4ba19ef87e7aa1c8b0321f3830ada075f84fca8a
Pbrown1404 <peterbrown140406@gmail.com>
2025-11-04 10:29:53 +0000

    Finished transactions logic, needs to be connected to html so it can be rendered
    # Conflicts:
    #	src/main/java/org/project/cardflex/App.java

--- src/main/java/org/project/cardflex/App.java
+++ src/main/java/org/project/cardflex/App.java
@@ -1,12 +1,7 @@
 package org.project.cardflex;
 
 import io.javalin.Javalin;
-import org.project.cardflex.Repository.CardRepository;
 import org.project.cardflex.Repository.TransactionsRepository;
-
-import javax.smartcardio.Card;
-import java.io.InputStream;
-import java.util.List;
 
 import static io.javalin.apibuilder.ApiBuilder.path;
 
@@ -21,27 +16,6 @@ public class App {
     }
 
     public App() {
-        app = Javalin.create(config -> {
-
-        });
-
-        app.get(
-                "/cards/{userId}",
-                ctx -> {
-                    var id = Integer.parseInt(ctx.pathParam("userId"));
-                    var cards = CardRepository.findCardsByUserID(id);
-                    ctx.json(cards);
-                }
-        );
-
-        app.post(
-                "/cards/{cardId}",
-                ctx ->{
-                    var id = Integer.parseInt(ctx.pathParam("cardId"));
-                    CardRepository.newCreditCard(id, "GOLD");
-                }
-        );
-
-
+        app = Javalin.create(config -> {});
     }
 }
